dcc1b70f5ac000951900be642a08fc5242ebcf8f
回锅鱼 <zhangcunchao@521php.com>
2021-04-16 15:32:47 +0800

    feat:access日志,日志切割

--- go.mod
+++ go.mod
@@ -6,4 +6,11 @@ require (
 	github.com/Unknwon/goconfig v0.0.0-20200908083735-df7de6a44db8 // indirect
 	github.com/gin-gonic/gin v1.6.3 // indirect
 	github.com/go-sql-driver/mysql v1.6.0 // indirect
+	github.com/lestrrat-go/file-rotatelogs v2.4.0+incompatible // indirect
+	github.com/lestrrat-go/strftime v1.0.4 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/rifflock/lfshook v0.0.0-20180920164130-b9218ef580f5 // indirect
+	github.com/sirupsen/logrus v1.8.1 // indirect
+	gorm.io/driver/mysql v1.0.5 // indirect
+	gorm.io/gorm v1.21.7 // indirect
 )
--- routers/routers.go
+++ routers/routers.go
@@ -2,12 +2,18 @@ package routers
 
 import (
 	"net/http"
+	"os"
+	"path"
+	"time"
 
 	"go-gateway/controller/admin"
 	"go-gateway/debug"
 	"go-gateway/inc"
 
 	"github.com/gin-gonic/gin"
+	rotatelogs "github.com/lestrrat-go/file-rotatelogs"
+	"github.com/rifflock/lfshook"
+	"github.com/sirupsen/logrus"
 )
 
 type Proxy struct {
@@ -43,20 +49,14 @@ func entrance(c *gin.Context) {
 	c.String(http.StatusOK, "hello World! %s", name)
 }
 
-func abc(c *gin.Context) {
-	name := c.FullPath()
-	c.String(http.StatusOK, "hello World! %s", name)
-}
-
 func InitRouter() *gin.Engine {
+
 	r := gin.New()
+
+	r.Use(logerMiddleware())
 
 	gwEntrance := inc.Cfg.MustValue("http", "GwEntrance", "api")
 	r.Any("/"+gwEntrance+"/*action", entrance)
-
-	test := "/abcd/:id"
-
-	r.Any(test, abc)
 
 	debug.DebugPrint("gateway entrance %s", gwEntrance)
 	//管理后台入口
@@ -72,4 +72,87 @@ func InitRouter() *gin.Engine {
 	}
 
 	return r
+}
+
+func logerMiddleware() gin.HandlerFunc {
+
+	// 日志文件
+	logPath := inc.Cfg.MustValue("http", "logPath", "storage/logs/access") // 日志打印到指定的目录
+	logMaxAge := inc.Cfg.MustInt("http", "logMaxAge", 15)
+
+	fileName := path.Join(logPath, "access.log")
+
+	//禁止logrus的输出
+	src, err := os.OpenFile(os.DevNull, os.O_APPEND|os.O_WRONLY, os.ModeAppend)
+	if err != nil {
+		debug.DebugPrint("gg-gin日志异常", err)
+	}
+	// 实例化
+	logger := logrus.New()
+	//设置日志级别
+	logger.SetLevel(logrus.DebugLevel)
+	//logger.SetFormatter(&logrus.JSONFormatter{})
+	//设置输出
+	logger.Out = src
+
+	// 设置 rotatelogs
+	logWriter, err := rotatelogs.New(
+		// 分割后的文件名称
+		fileName+".%Y-%m-%d",
+
+		// 生成软链，指向最新日志文件
+		rotatelogs.WithLinkName(fileName),
+
+		// 设置最大保存时间(7天)
+		rotatelogs.WithMaxAge(time.Duration(logMaxAge)*24*time.Hour),
+
+		// 设置日志切割时间间隔(1天)
+		rotatelogs.WithRotationTime(24*time.Hour),
+	)
+
+	if err != nil {
+		debug.DebugPrint("gg-gin日志异常", err)
+	}
+
+	writeMap := lfshook.WriterMap{
+		logrus.InfoLevel:  logWriter,
+		logrus.FatalLevel: logWriter,
+		logrus.DebugLevel: logWriter,
+		logrus.WarnLevel:  logWriter,
+		logrus.ErrorLevel: logWriter,
+		logrus.PanicLevel: logWriter,
+	}
+
+	logger.AddHook(lfshook.NewHook(writeMap, &logrus.JSONFormatter{
+		TimestampFormat: "2006-01-02 15:04:05",
+	}))
+
+	return func(c *gin.Context) {
+		//开始时间
+		startTime := time.Now()
+		//处理请求
+		c.Next()
+		//结束时间
+		endTime := time.Now()
+		// 执行时间
+		latencyTime := endTime.Sub(startTime)
+		//请求方式
+		reqMethod := c.Request.Method
+		//请求路由
+		reqUrl := c.Request.RequestURI
+		//状态码
+		statusCode := c.Writer.Status()
+		//请求ip
+		clientIP := c.ClientIP()
+
+		// 日志格式
+		logger.WithFields(logrus.Fields{
+			"status_code":  statusCode,
+			"latency_time": latencyTime,
+			"client_ip":    clientIP,
+			"req_method":   reqMethod,
+			"req_uri":      reqUrl,
+		}).Info()
+
+	}
 }
